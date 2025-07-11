685fb42683dfef172b78df5e33a09916776a0e65
Dhanyashree Gurukar <dhanyashreesgurukar@gmail.com>
2025-07-11 22:49:13 +0530

    Initial commit

---
+++ Lab10/a. Date/date.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Date</title>
+        <style>
+            h1 {
+                text-align: center;
+            }
+            date {
+                color: green;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Today's date is <date>
+            <?php
+                $today = date("d-M-Y");
+                echo $today;
+            ?></date>
+        </h1>
+    </body>
+</html>
